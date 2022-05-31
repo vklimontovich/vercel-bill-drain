157296ab365b05c996e4c3cc3ad1b72b9ade1279
Vladimir Klimontovich <vladimir@jitsu.com>
2022-05-30 20:28:05 -0400

    Added endless redirect

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -9,5 +9,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(`${req.method} ${req.url}. Headers: ${JSON.stringify(req.headers)}`)
+  if (req.query?.step === '0') {
+    res.redirect("/api/redirect?step=1")
+  } else {
+    res.redirect("/api/redirect?step=0")
+  }
 }
